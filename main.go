46a54d3abecb1f05ccb59e288317b77002d46a30
xing.tang <xing.tang@rrd.com>
2020-04-05 22:00:01 +0800

    logrus

--- main.go
+++ main.go
@@ -3,10 +3,14 @@ package main
 import (
 	"github.com/caspertu/book/api/v1/books"
 	"github.com/gin-gonic/gin"
+	"github.com/sirupsen/logrus"
 )
+
+var log = logrus.New()
 
 func main() {
 	r := gin.Default()
 	books.SetupRouter(r)
-	r.Run()
+	log.Info("Server started")
+	log.Fatalln(r.Run())
 }
