370cbe63bdf2477acdd9a89892b5d4838da85936
xing.tang <xing.tang@rrd.com>
2020-04-10 19:57:19 +0800

    HTML templates

--- main.go
+++ main.go
@@ -4,12 +4,19 @@ import (
 	"github.com/caspertu/book/api/v1/books"
 	"github.com/gin-gonic/gin"
 	"github.com/sirupsen/logrus"
+	"net/http"
 )
 
 var log = logrus.New()
 
 func main() {
 	r := gin.Default()
+	r.LoadHTMLGlob("tmpl/*")
+	r.GET("/", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "index.tmpl", gin.H{
+			"title": "Main website",
+		})
+	})
 	books.SetupRouter(r)
 	log.Info("Server started")
 	log.Fatalln(r.Run())
